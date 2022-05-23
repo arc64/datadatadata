478e8de6fc7394463445f77606f21e5eea425295
Annabel Church <215145+arc64@users.noreply.github.com>
2022-05-23 20:37:51 +1200

    ]Init: test video on canvas with structured files

---
+++ assets/js/canvas.js
@@ -0,0 +1,37 @@
+
+ const canvasDimentions = (canvas) => {
+	let dpr = window.devicePixelRatio           // device pixels per CSS pixel (can be fractional)
+	let cssWidth = canvas.clientWidth           // CSS dimensions of canvas
+	let cssHeight = canvas.clientHeight
+	let pxWidth = Math.round(dpr * cssWidth)    // dimensions we should set the backing buffer to
+	let pxHeight = Math.round(dpr * cssHeight)
+	return {dpr, cssWidth, cssHeight, pxWidth, pxHeight}
+  }
+
+  const redraw = (canvas, draw) => {
+	  console.log("redraw")
+	  // DPI-corrected CSS dimensions
+	  let {dpr, cssWidth, cssHeight, pxWidth, pxHeight} = canvasDimentions(canvas)
+	  canvas.width = pxWidth
+	  canvas.height = pxHeight
+
+	  let ctx = canvas.getContext('2d')
+	  ctx.scale(dpr, dpr)
+	  // draw videos
+	  console.log("redraw", dpr)
+	  draw(ctx, canvas);
+  }
+
+  const debugCanvas = (context, videoElement) => {
+	context.fillStyle = "#000000";
+	context.fillText  ("Duration:" + videoElement.duration,  10 ,280);
+	context.fillText  ("Current time:" + videoElement.currentTime,  260 ,280);
+	context.fillText  ("Loop: " + videoElement.loop,  10 ,290);
+	context.fillText  ("Autoplay: " + videoElement.autoplay,  100 ,290);
+	context.fillText  ("Muted: " + videoElement.muted,  180 ,290);
+	context.fillText  ("Controls: " + videoElement.controls,  260 ,290);
+	context.fillText  ("Volume: " + videoElement.volume,  340 ,290);
+
+  }
+
+  export { canvasDimentions, redraw, debugCanvas };
--- index.md
+++
@@ -1,8 +0,0 @@
----
-layout: default
-title: "Coming soon: An Ontological Experience on Data Fluidity"
-byline: "Created by Sherry Wong and Caroline Sinders"
----
-
-
-
